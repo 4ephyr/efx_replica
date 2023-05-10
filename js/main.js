88e03f06f76ee90f9598b687f2f42cafa220db0d
Mohammed Sahal <technicalsahal25@gmail.com>
2023-05-10 17:47:38 +0530

    Added transition to mobile icon menu

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ let nav_icon = document.getElementById('test-nav')
 let mobilenav = document.getElementById('mobilenav')
 let nav_small = document.getElementById('nav-small')
 let links = document.getElementsByClassName('link1')
+let section = document.getElementById('sec1')
+let header = document.getElementById('main-header')
 
 nav_icon.addEventListener('click',()=>{
     mobilenav.classList.toggle("sample")
@@ -14,3 +16,9 @@ for(let i = 0; i < links.length; i++) {
         nav_small.classList.remove('sample')
     });
 }
+sec1.addEventListener('click',()=>{
+    if(mobilenav.className.match('sample') && nav_small.className.match('sample')){
+        nav_small.classList.remove('sample')
+        mobilenav.classList.remove('sample')
+    }
+})
